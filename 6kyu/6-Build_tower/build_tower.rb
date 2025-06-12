b6c441c921119b4d2fa4b4a38ecfb27fcbf407df
laurent papinaud <laurent.papinaud@gmail.com>
2025-06-12 16:37:44 +0200

    use map on array with n numbers and find the pattern of building

--- 6kyu/6-Build_tower/build_tower.rb
+++ 6kyu/6-Build_tower/build_tower.rb
@@ -1,21 +1,26 @@
-Build Tower
-Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors.
-A tower block is represented with "*" character.
+def towerBuilder(n_floors)
+  # tower = []
+  # nbr_of_spaces = 0
+  # space_caract = " "
+  # dot_caract = "*"
+  # impairs = (1..n_floors*2).step(2).first(n_floors)
 
-For example, a tower with 3 floors looks like this:
-[
-  "  *  ",
-  " *** ",
-  "*****"
-]
 
-And a tower with 6 floors looks like this:
+  # impairs.each_with_index do |nbr_of_dots, index|
+  #   floor = ""
+  #   nbr_of_spaces = n_floors - index -1
+  #   nbr_of_spaces.times {floor << space_caract}
+  #   nbr_of_dots.times {floor << dot_caract}
+  #   nbr_of_spaces.times {floor << space_caract}
+  #   tower << floor
 
-[
-  "     *     ",
-  "    ***    ",
-  "   *****   ",
-  "  *******  ",
-  " ********* ",
-  "***********"
-]
+  (1..n_floors).map do |i|
+    space = ' ' * (n_floors - i)
+    stars = '*' * (i * 2 - 1)
+    space + stars + space
+  end
+
+  # return tower
+end
+
+puts towerBuilder(5)
--- exercice.rb
+++
@@ -1,3 +0,0 @@
-def towerBuilder(n_floors)
-  return []
-end
