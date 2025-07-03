d9c213593f9a5ddb389ce4c46a069bc17ccda8f6
laurent papinaud <laurent.papinaud@gmail.com>
2025-07-03 11:08:12 +0200

    get max value from dictionary of trays whit lambda function, result is a tupple with tray index and value

---
+++ Python/coderpad/3-sort_boxes/instruction.md
@@ -0,0 +1,3 @@
+Objectif; sélectionner les colis du plus lourd au plus léger
+
+Implémenter la fonction solve(weight_0, weight_1, weight_2) qui prend en argument 3 entiers. Ces valeurs représentent le poids des colisprésnet respectivement sur les tapis d'incice 0 ,1 ,2. Lorsqu'un tapis est vide, la valeur envoyée est 0
--- Python/coderpad/3-sort_boxes/sort_boxes.py
+++ Python/coderpad/3-sort_boxes/sort_boxes.py
@@ -5,27 +5,32 @@ from contextlib import redirect_stdout
 
 def solve(weight_0, weight_1, weight_2):
     dict_of_trays = {1:weight_0, 2:weight_1, 3:weight_2}
-    selected_parcel = ""
-    # Write your code here
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    selected_parcel = max(dict_of_trays.items(), key = lambda tray: tray[1])
     print(selected_parcel)
-    return selected_parcel
+    return selected_parcel[0]
+
+
+weight_1 = 130
+weight_2 = 50
+weight_3 = 200
+
+print(solve(weight_1, weight_2, weight_3))
 
 
 # Ignore and do not change the code below
-def main():
-    # pylint: disable = C, W
+# def main():
+#     # pylint: disable = C, W
 
-    # game loop
-    while True:
-        weight_0 = int(input())
-        weight_1 = int(input())
-        weight_2 = int(input())
-        with redirect_stdout(sys.stderr):
-            action = solve(weight_0, weight_1, weight_2)
-        print(action)
+#     # game loop
+#     while True:
+#         weight_0 = int(input())
+#         weight_1 = int(input())
+#         weight_2 = int(input())
+#         with redirect_stdout(sys.stderr):
+#             action = solve(weight_0, weight_1, weight_2)
+#         print(action)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 # Ignore and do not change the code above
